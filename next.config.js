48668f575ec13066f0d02d5dbe5c63364d7e03da
Simon Taisne <simon.taisne@gmail.com>
2023-03-13 14:35:31 +0100

    Install SVGR and configure Next.js to import SVG as React components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // @see https://react-svgr.com/docs/next/
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
